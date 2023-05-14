3870fd93913c33a0cc3fb1b52f0076f05a373e99
skp544 <shubhamkumarprajapati544@gmail.com>
2023-05-14 11:32:20 +0530

    progress-bar

---
+++ progress-bar/script.js
@@ -0,0 +1,18 @@
+function start() {
+  const progress = document.querySelector(".progress");
+
+  let width = 0;
+
+  const timeInterval = setInterval(fill, 20);
+  function fill() {
+    if (width >= 80) {
+      clearInterval(timeInterval);
+    } else {
+      width++;
+      progress.style.width = `${width}%`;
+      progress.innerHTML = `${width}%`;
+    }
+  }
+}
+
+window.onload = () => start();
