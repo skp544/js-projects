fc3f3dd4555bae6371028014387d2f6fea51fcce
skp544 <shubhamkumarprajapati544@gmail.com>
2023-05-29 19:05:00 +0530

    image-gallery-filter

---
+++ image-gallery-filter/script.js
@@ -0,0 +1,21 @@
+const galleryFilter = document.querySelector(".gallery-filter");
+const galleryImages = document.querySelectorAll(".image");
+
+galleryFilter.addEventListener("click", (e) => {
+  if (e.target.classList.contains("filter-gallery")) {
+    galleryFilter.querySelector(".active").classList.remove("active");
+
+    e.target.classList.add("active");
+
+    const filter = e.target.dataset.filter;
+    console.log(filter);
+
+    galleryImages.forEach((image) => {
+      if (filter == "all" || image.classList.contains(filter)) {
+        image.style.display = "block";
+      } else {
+        image.style.display = "none";
+      }
+    });
+  }
+});
